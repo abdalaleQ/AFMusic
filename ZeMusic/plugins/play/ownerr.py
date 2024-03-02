11b5a7c1929d9ed96e6a12583eefdd07f4c78165
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2024-03-02 04:32:08 +0300

    Update ownerr.py

--- ZeMusic/plugins/play/ownerr.py
+++ ZeMusic/plugins/play/ownerr.py
@@ -74,7 +74,7 @@ async def kimmyy(client: Client, message: Message):
     usr = await client.get_users(OWNER)
     name = usr.first_name
     async for photo in client.iter_profile_photos(OWNER, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""**ᯓ 「𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾」، ⦃𓏛**
+                    await message.reply_photo(photo.file_id,       caption=f"""**ᯓ 「سورس الزعيم」، ⦃𓏛**
                     
 🔥 ¦𝚆𝙾𝙽𝙴𝚁 :[{usr.first_name}](https://t.me/{OWNER})
 📀 ¦𝚄𝚂𝙴𝚁 :@{OWNER} 
