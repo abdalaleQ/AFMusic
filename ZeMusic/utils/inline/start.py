e7bdff658880c734ea9d85de4915d729b6a81049
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2024-03-02 06:26:00 +0300

    Update start.py

--- ZeMusic/utils/inline/start.py
+++ ZeMusic/utils/inline/start.py
@@ -8,18 +8,18 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᗩᗪᗪ ᗰE TO YOᑌᖇ GᖇOᑌᑭ",
+                text="اضف البوت الى مجموعتك",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [InlineKeyboardButton(text="الأوامر", callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text="ᗪEᐯEᒪOᑭEᖇ", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="ᑕᕼᗩᑎᑎEᒪ", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="مطور السورس", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="جروب السورس", url=config.SUPPORT_CHANNEL),
         ],
         [
          
-            InlineKeyboardButton(text="سورس ميوزك", url=f"https://t.me/T_5_G"),
+            InlineKeyboardButton(text="سورس الزعيم", url=f"https://t.me/T_5_G"),
         ],
     ]
     return buttons
@@ -29,18 +29,18 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᗩᗪᗪ ᗰE TO YOᑌᖇ GᖇOᑌᑭ",
+                text="اضف البوت الا مجموعتك",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [InlineKeyboardButton(text="الأوامر", callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text="ᗪEᐯEᒪOᑭEᖇ", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="ᑕᕼᗩᑎᑎEᒪ", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="مطور السورس", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="جروب السورس", url=config.SUPPORT_CHANNEL),
         ],
         [
          
-            InlineKeyboardButton(text="سورس ميوزك", url=f"https://t.me/VVV5P"),
+            InlineKeyboardButton(text="سورس الزعيم", url=f"https://t.me/EEEW2"),
         ],
     ]
     return buttons
