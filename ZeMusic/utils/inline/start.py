23cd7bcb01370fe0245b8853528bf20787e3e0b3
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2024-03-04 16:14:44 +0300

    Update start.py

--- ZeMusic/utils/inline/start.py
+++ ZeMusic/utils/inline/start.py
@@ -8,14 +8,14 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᗩᗪᗪ ᗰE TO YOᑌᖇ GᖇOᑌᑭ",
+                text="اضف البوت الى مجموعتك",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [InlineKeyboardButton(text="الأوامر", callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text="ᗪEᐯEᒪOᑭEᖇ", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="ᑕᕼᗩᑎᑎEᒪ", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="مطور السورس", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="قناة السورس", url=config.SUPPORT_CHANNEL),
         ],
         [
          
@@ -29,14 +29,14 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᗩᗪᗪ ᗰE TO YOᑌᖇ GᖇOᑌᑭ",
+                text="اضف البوت الى مجموعتك",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [InlineKeyboardButton(text="الأوامر", callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text="ᗪEᐯEᒪOᑭEᖇ", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="ᑕᕼᗩᑎᑎEᒪ", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="مطور السورس", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="قناة السورس", url=config.SUPPORT_CHANNEL),
         ],
         [
          
